ca0e8e5cbce63b9f46030da3e2654d83ba1015af
Nathan Gobinet <nathan.gobinet@gmail.com>
2021-11-01 22:30:23 -0400

    Set backgroud properly

--- src/DrawEngine.tsx
+++ src/DrawEngine.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { CSSProperties, useEffect, useRef, useState } from 'react';
 import { Engine, drawState, ShapeType } from '../../memo-draw-engine/src/index';
 import ColorBar from './Components/ColorBar';
 import OpacityBar from './Components/OpacityBar';
@@ -7,25 +7,37 @@ import ThicknessBar from './Components/ThicknessBar';
 import NetworkManager from './Services/NetworkManager';
 
 function DrawEngine() {
-  const canvasRef = useRef();
-  let engine : Engine;
+  const [backgroundStyle, setBackgroundStyle] = useState<CSSProperties>();
+  const canvasRef = useRef<HTMLCanvasElement>();
+  let engine: Engine;
 
   useEffect(() => {
     engine = new Engine(canvasRef.current, new NetworkManager());
     engine.eventManager.registerDefaultCanvasAndDocumentEvents();
     drawState.shapeType = ShapeType.Pencil;
     drawState.thickness = 5;
-  }, []);  
+    initBackgroundStyle();
+  }, []);
+
+  const initBackgroundStyle = (): void => {
+    setBackgroundStyle({
+      position: 'absolute',
+      backgroundColor: 'white',
+      zIndex: -2,
+      width: `${canvasRef.current.width}px`,
+      height: `${canvasRef.current.height}px`,
+    });
+  };
 
   return (
     <div>
+      <div style={backgroundStyle}></div>
       <canvas width="800" height="600" ref={canvasRef}></canvas>
       <ColorBar />
       <ShapeBar />
       <OpacityBar />
       <ThicknessBar />
     </div>
-    
   );
 }
 
